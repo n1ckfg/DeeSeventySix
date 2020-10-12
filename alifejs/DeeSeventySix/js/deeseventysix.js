24f311c975853262c26e7e3e10383a37de1a868f
Nick Fox-Gieg <nick@fox-gieg.com>
2020-10-11 21:02:27 -0400

    testing

--- alifejs/DeeSeventySix/js/deeseventysix.js
+++ alifejs/DeeSeventySix/js/deeseventysix.js
@@ -3,29 +3,35 @@
 let darkroom;  // Darkroom
 let toggleImg;  // bool
 const isColor = false;
-let img;  // PImage
+let ready = false;
+let img;
+let url  = loadImage("./images/blockbuster_small.jpg");
 
-function preload() {
-    img = loadImage("./images/blockbuster_small.jpg");
+function reset() {
+    img = new field2D(256, 256);
+    img.loadiMage(url, function() { 
+        darkroom = new Darkroom(img, isColor);
+        //createCanvas(darkroom.frame.width, darkroom.frame.height);
+        
+        toggleImg = false;
+        
+        darkroom.expose();
+
+        ready = true;
+    });
+
 }
 
-function setup() {
-    darkroom = new Darkroom(img, isColor);
-    createCanvas(darkroom.frame.width, darkroom.frame.height);
-    
-    toggleImg = false;
-    
-    darkroom.expose();
+function update(dt) {    
+    if (ready) darkroom.develop();
 }
 
 function draw() {
-    background(0);
-    
-    darkroom.develop();
-    
-    if (toggleImg) {
-        darkroom.drawSource();
-    } else {
-        darkroom.draw();
-    }    
+    if (ready) {
+        if (toggleImg) {
+            darkroom.drawSource();
+        } else {
+            darkroom.draw();
+        }    
+    }
 }
