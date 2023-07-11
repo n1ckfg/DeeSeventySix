5a8d51ee10f16557d38cb4020bea137a8f9589f6
Nick Fox-Gieg <nick@fox-gieg.com>
2023-07-11 10:11:04 -0400

    reorganize

--- alifejs/DeeSeventySix/js/msg.js
+++ alifejs/DeeSeventySix/js/msg.js
@@ -2,7 +2,9 @@
 
 let msg;
 
-let controlinfo = "<br><br>C: Colour<br>D: Diffuse<br>N: Normalize<br>S: Save image<br>space: source image";
+const controlinfo = "<br><br>C: Colour<br>D: Diffuse<br>N: Normalize<br>S: Save image<br>space: source image";
+
+const linkinfo = "<br><br><a href='../../ml5js/DeeSeventySix'>ml5.js</a>&nbsp;&nbsp;&nbsp;&nbsp;<a href='../../p5js/DeeSeventySix'>p5.js</a>"
 
 window.onload = function() {
     msg = document.getElementById("msg");
@@ -10,7 +12,7 @@ window.onload = function() {
     if (!util.checkForMouse()) {
     	msg.innerHTML = "reload for demo<br>more options on desktop";
     } else {
-    	msg.innerHTML = "press enter for demo<br>or drag-drop an image" + controlinfo;
+    	msg.innerHTML = "press enter for demo<br>or drag-drop an image" + controlinfo + linkinfo;
     }
 
     document.addEventListener('keydown', function() {    	
