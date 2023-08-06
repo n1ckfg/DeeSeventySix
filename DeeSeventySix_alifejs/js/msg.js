b2913b9221feab48dc97d47f2f45e001b60e4824
Nick Fox-Gieg <nick@fox-gieg.com>
2023-08-06 18:44:46 -0400

    reorganize submodules

--- DeeSeventySix_alifejs/js/msg.js
+++ DeeSeventySix_alifejs/js/msg.js
@@ -4,7 +4,7 @@ let msg;
 
 const controlinfo = "<br><br>C: Colour<br>D: Diffuse<br>N: Normalize<br>S: Save image<br>space: source image";
 
-const linkinfo = "<br><br><a href='../../ml5js/DeeSeventySix'>ml5.js</a>&nbsp;&nbsp;&nbsp;&nbsp;<a href='../../p5js/DeeSeventySix'>p5.js</a>"
+const linkinfo = "<br><br><a href='../../DeeSeventySix_ml5js/'>ml5.js</a>&nbsp;&nbsp;&nbsp;&nbsp;<a href='../../DeeSeventySix_p5js/'>p5.js</a>"
 
 window.onload = function() {
     msg = document.getElementById("msg");
