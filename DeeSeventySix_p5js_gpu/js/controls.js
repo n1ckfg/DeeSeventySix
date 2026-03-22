c3925f4e311ca091472ce2d7b194d11a591f7628
Nick Fox-Gieg <nick@fox-gieg.com>
2026-03-22 00:14:43 -0400

    shader

--- DeeSeventySix_p5js_gpu/js/controls.js
+++ DeeSeventySix_p5js_gpu/js/controls.js
@@ -8,5 +8,24 @@ function keyPressed() {
         case ' ':
             toggleImg = !toggleImg;
             break;
-    } 
+        case 'g':
+            grainScale = max(10.0, grainScale - 10.0);
+            console.log("grainScale: " + grainScale);
+            break;
+        case 'G':
+            grainScale = min(500.0, grainScale + 10.0);
+            console.log("grainScale: " + grainScale);
+            break;
+        case 'o':
+            solarizeLimit = max(1.0, solarizeLimit - 5.0);
+            console.log("solarizeLimit: " + solarizeLimit);
+            break;
+        case 'O':
+            solarizeLimit = min(200.0, solarizeLimit + 5.0);
+            console.log("solarizeLimit: " + solarizeLimit);
+            break;
+        case 'r':
+            progress = 0;
+            break;
+    }
 }
--- DeeSeventySix_p5js_gpu/js/main.js
+++ DeeSeventySix_p5js_gpu/js/main.js
@@ -5,6 +5,10 @@ let toggleImg;
 let img;
 let progress = 0;
 
+// Grain and solarization parameters
+let grainScale = 100.0;    // Voronoi cell density - higher = smaller grains
+let solarizeLimit = 90.0;  // Overexposure threshold (matches CPU default)
+
 function preload() {
     img = loadImage("./images/blockbuster_mid.jpg");
     theShader = loadShader('shader.vert', 'shader.frag');
@@ -13,26 +17,28 @@ function preload() {
 function setup() {
     createCanvas(img.width, img.height, WEBGL);
     noStroke();
-    
+
     toggleImg = false;
 }
 
 function draw() {
     background(0);
-    
+
     if (toggleImg) {
         resetShader();
         image(img, -width/2, -height/2, width, height);
     } else {
         progress += 0.005; // Simulate developing time
         if (progress > 1.0) progress = 1.0;
-        
+
         shader(theShader);
         theShader.setUniform('u_tex', img);
         theShader.setUniform('u_resolution', [width, height]);
         theShader.setUniform('u_progress', progress);
         theShader.setUniform('u_isColor', true);
-        
+        theShader.setUniform('u_grainScale', grainScale);
+        theShader.setUniform('u_solarizeLimit', solarizeLimit);
+
         rect(-width/2, -height/2, width, height);
-    }    
+    }
 }
